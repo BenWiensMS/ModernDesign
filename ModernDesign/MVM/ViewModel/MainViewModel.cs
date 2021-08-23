7f11516a05f15ced1b77a2f5047c2014edd70f82
Benjamin Wiens <BWiens@fiege.com>
2021-08-24 00:55:29 +0200

    preparation of WPF add new classes and attirbutes

---
+++ ModernDesign/MVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ModernDesign.core;
+
+namespace ModernDesign.MVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
